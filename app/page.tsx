4bee25407a825ec6e651719ce5da62f3aaf5b78d
Yuvraj <2604yuvraj@gmail.com>
2023-07-19 20:31:12 +0100

    shadcn ui set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className="text-6xl text-green-500">Hello</p>
+    <Button>Click me</Button>
   )
 }
