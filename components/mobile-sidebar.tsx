804f103a9a61f146eaaa9743f1c95916de60b1f1
Yuvraj <2604yuvraj@gmail.com>
2023-07-22 00:58:09 +0100

    feature: mobile sidebar using sheet component with toggle

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,12 +3,25 @@
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger 
+} from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
 
 const MobileSidebar = () => {
     return (
-        <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu />
-        </Button>
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>           
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
     );
 }
 
