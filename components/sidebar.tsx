3325cb81e2f16682a5a7f1e93abda82fd79c30a6
Yuvraj <2604yuvraj@gmail.com>
2023-07-21 21:51:08 +0100

    sidebar styling and logo positioning

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-// 
+import Image from "next/image";
+import Link from "next/link";
+
+// Sidebar styling and logo positioning
 const Sidebar =() => {
     return (
         <div className="space-y-4 py-4 flex flex-col h-full
         bg-[#111827] text-white">
-            Hello Sidebar Component
+            <div className="px-3 py-2 flex-1">
+                <Link href="/dashboard" className="flex
+                items-center pl-3 mb-14">
+                    <div className="relative w-8 h-8 mr-4">
+                        <Image
+                            fill
+                            alt="Logo"
+                            src="/logo.png"
+                        />
+                    </div>
+                </Link>
+
+            </div>
         </div>
     );
 }
