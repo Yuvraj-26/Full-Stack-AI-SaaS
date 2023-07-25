56c5fbbf30c826fa3ae3d82a8b079a316a52e458
Yuvraj <2604yuvraj@gmail.com>
2023-07-25 01:51:40 +0100

    feature: Code Generation API OpenAI modal and route

---
+++ app/api/code/route.ts
@@ -0,0 +1,55 @@
+// required imports
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// instruction message tells the OpenAI model to become a code generator using system role
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: "You are a code generator. You must answer only in markdown code snippets. Always use code comments for explanations."
+}
+
+export async function POST(
+    req: Request
+) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { messages } = body;
+
+        // check for authentication
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        // check for openAI API key configuration
+        if (!configuration.apiKey) {
+            return new NextResponse("OpenAI API Key not configured", { status: 500 });
+        }
+
+        // check if messages have not been passed to the route
+        if (!messages) {
+            return new NextResponse("Messages are required", { status: 400 });
+        }
+
+        // open AI modal usage, starts with instruction message 
+        // to turn AI model into code generator, followed by all other messages
+        
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [instructionMessage, ...messages]
+        });
+
+
+        return NextResponse.json(response.data.choices[0].message);
+    } catch (error) {
+        console.log("CODE_ERROR", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
